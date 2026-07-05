185913134d64284f9339a32d6b08fa926213129c
mrko <nahsuhk1111@gmail.com>
2026-07-05 23:06:28 +0545

    favicon added

--- jsFiles/layout_Wrapper.js
+++ jsFiles/layout_Wrapper.js
@@ -19,3 +19,18 @@ function replaceContent() {
         .catch(error => console.error(error));
 }
 replaceContent();
+
+function setFavicon(url) {
+    let link = document.querySelector("link[rel~='icon']");
+    
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+    }
+    
+    link.href = url; 
+    link.type = 'image/png';
+}
+
+setFavicon('/photos/bamboograss.png');
